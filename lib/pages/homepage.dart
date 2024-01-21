86dc928e99da3320e35d1201635e94349f178111
Yannis <yannisamzal@gmail.com>
2024-01-21 08:24:23 +0100

    refactor: remove underscore from _HomePageState

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -7,10 +7,10 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomePage> createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   final nameController = TextEditingController();
   final horaireBrutController = TextEditingController();
   final mensuelBrutController = TextEditingController();
